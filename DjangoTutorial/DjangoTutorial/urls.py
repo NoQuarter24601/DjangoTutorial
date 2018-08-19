0679123392ccaaaa1af11b22297e3c429d5f5fe2
Joshua Bessell-Browne <jbessellbrowne@gmail.com>
2018-08-19 20:58:45 +0800

    Added music app

--- DjangoTutorial/DjangoTutorial/urls.py
+++ DjangoTutorial/DjangoTutorial/urls.py
@@ -3,19 +3,22 @@ Definition of urls for DjangoTutorial.
 """
 
 from django.conf.urls import include, url
-
+from django.contrib import admin
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^music/', include('music.urls')),
+
     # Examples:
     # url(r'^$', DjangoTutorial.views.home, name='home'),
     # url(r'^DjangoTutorial/', include('DjangoTutorial.DjangoTutorial.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     #url(r'^admin/', include(admin.site.urls)),
 ]
